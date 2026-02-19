4733acd04b92f8a19035b88b0f24aa77084c8ae5
DiscipleA <132625970+DiscipleA@users.noreply.github.com>
2026-02-19 00:12:05 -0800

    gatsby push
    deleted .gitignore

---
+++ Week6/gatsby-framework-assignment/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/reader/Desktop/AD311/AD311_HW_Code/Week6/gatsby-framework-assignment/.cache/dev-404-page.js")),
+  "component---src-pages-404-tsx": preferDefault(require("/home/reader/Desktop/AD311/AD311_HW_Code/Week6/gatsby-framework-assignment/src/pages/404.tsx")),
+  "component---src-pages-index-tsx": preferDefault(require("/home/reader/Desktop/AD311/AD311_HW_Code/Week6/gatsby-framework-assignment/src/pages/index.tsx"))
+}
+
